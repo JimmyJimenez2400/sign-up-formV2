e08a3c0513b8f6668930635c3d838e5d624ab476
Jimmy Jimenez <jimmy.jimenez@jjay.cuny.edu>
2023-01-01 17:52:10 -0500

    Test DOM Of Password And ConfirmPassword

---
+++ app.js
@@ -0,0 +1,8 @@
+const password = document.getElementById('pwd');
+const confirmPassword = document.getElementById('confirmPwd');
+
+
+console.log(password.value);
+console.log(confirmPassword.value);
+
+
